38d3debd86ee4c4996bcc43ce92370b0fa2e23dc
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-11-13 22:46:12 +0100

    Registry Eureka Service: Création du serveur Eureka pour la découverte des micro-services et test de l'enregistrement

---
+++ discovery-service/src/test/java/ma/elamrani/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.elamrani.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
