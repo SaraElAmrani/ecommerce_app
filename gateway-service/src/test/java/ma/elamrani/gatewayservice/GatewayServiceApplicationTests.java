c022ad16969192907b95e2290fb40edb80003793
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-11-13 22:45:48 +0100

    Gateway Service: Mise en place de Spring Cloud Gateway, configuration statique et test du proxy vers les micro-services

---
+++ gateway-service/src/test/java/ma/elamrani/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.elamrani.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
