ff58d9d8ec0345191c06cd2e880cde9e1372a50f
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-11-13 22:44:13 +0100

    Inventory Service: Création de l'entité Product, repository Spring Data, endpoints REST CRUD et tests

---
+++ inventory-service/src/main/java/ma/elamrani/inventoryservice/config/RestRepositoryConfig.java
@@ -0,0 +1,15 @@
+package ma.elamrani.inventoryservice.config;
+
+import ma.elamrani.inventoryservice.entities.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestRepositoryConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Product.class);
+    }
+}
